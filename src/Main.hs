d6fdc6c30eca7b9a7364f38552b681df7c8d5f53
Ubuntu <ubuntu@ip-172-31-17-93.ap-southeast-2.compute.internal>
2020-06-11 13:54:19 +0000

    updated everything

---
+++ src/Main.hs
@@ -0,0 +1,23 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module Main where
+
+import           Web.Scotty
+import           Data.Text
+import           Network.HTTP.Types
+
+main :: IO ()
+main = scotty 3000 $ do
+  get "/health" $ text "OK"
+
+  post "/login" handleLogin
+
+handleLogin :: ActionM ()
+handleLogin = do
+  username <- param "user"
+  password <- param "password"
+  if login username password then text "Success" else status status403
+
+login :: Text -> Text -> Bool
+login "admin" "admin" = True
+login _       _       = False
